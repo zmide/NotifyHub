6cfd89b0bf56c31f74170b6a32860f7e3154cc06
JunXiaoRuo <47996900+JunXiaoRuo@users.noreply.github.com>
2025-08-13 01:00:39 +0800

    更新环境配置

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///database.db')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///database.db') # 开发环境
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:////var/lib/notifyhub/database.db') # 运行环境（linux）
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # 添加加密密钥配置
